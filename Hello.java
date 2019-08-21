2580b02905bb3e767d9b881e80fc470fae9fe4fb
whangt <54372981+whangt@users.noreply.github.com>
2019-08-21 14:05:07 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,22 @@
+/*Java 
+*Says Hello
+*___________________________
+*Taeyoung Whang
+*8/21/2019
+*CMSC 255 - 002*/
+
+
+public class Hello {
+ public static void printHeading() {
+  System.out.println("Taeyoung Whang");
+  System.out.println("Whangt Java project");
+  System.out.println("Says hello");
+  System.out.println("8/21/2019");
+  System.out.println();
+ }
+ 
+ public static void main(String[] args) {
+	printHeading();
+	System.out.println("Hello");
+	}
+}
